34bf8ddd8cf2479432309c5eadfa7dea63edc593
Jake Wehder <108289690+JWehder@users.noreply.github.com>
2023-10-17 18:18:00 -0400

    adding prompting to the app so the ai can answer the questions to build flashcards

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from tkinter import *
+from bs4 import BeautifulSoup
+import requests
 
-root = Tk()
-root.title('Automated Flashcards')
-root.geometry('550x410')
+headers = {'user-agent': 'flashcard-automation/0.0.1'}
 
+html = requests.get('https://www.interviewbit.com/python-interview-questions/', headers=headers)
 
-root.mainloop()
+doc = BeautifulSoup(html.text, 'html.parser')
+
+x = 0
+for _ in range(4):
+    data = doc.select('.ibpage-article-header')[x].select('h3')[0]
+    parsed_data = data.contents[0]
+    
+    x+=1
---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.title('Automated Flashcards')
+root.geometry('550x410')
+
+
+root.mainloop()
