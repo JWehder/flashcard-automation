2117ddc57767c9d1457d57ccb932da744596d64b
Jake Wehder <108289690+JWehder@users.noreply.github.com>
2023-10-22 15:53:49 -0400

    starting tkinter GUI

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CardCarousel from "./components/CardCarousel"
+import { CenterDiv } from "./styles/Styles";
+
+export default function App() {
+    return (
+        <CenterDiv>
+            <CardCarousel />
+        </CenterDiv>
+    )
+}
---
+++ client/src/styles/Styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const CenterDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Card = styled(div)`
+    width: 500;
+    height: 300;
+    text-align: center;
+    
+`
